8abc4dfcb242d4917be4361e461ca39cd1f8cac3
Sandro Spengler <Sandro.Spengler@web.de>
2021-12-02 14:20:55 +0100

    Deplyoment on Production

--- scripts/runDockerComposeProd.sh
+++ scripts/runDockerComposeProd.sh
@@ -1,21 +1,29 @@
 #1. Build and Runs the Docker-Compose Envoirment
 #2. Clears unnecessary images
-# CLEAR=$1
-
-docker-compose down --remove-orphans -v
+CLEAR=$1
 
 docker-compose \
-    -f ~/Docker/MainServer/docker-compose.yaml \
-    -f ~/Docker/MainServer/docker-compose.prod.yaml \
+    -f ${MainDockerPath}docker-compose.yaml \
+    -f ${MainDockerPath}docker-compose.prod.yaml \
+    -f ${ExpressMongoDBPath}docker-compose.yaml \
+    -f ${ExpressMongoDBPath}docker-compose.prod.yaml \
+    down --remove-orphans -v
+
+docker-compose \
+    -f ${MainDockerPath}docker-compose.yaml \
+    -f ${MainDockerPath}docker-compose.prod.yaml \
+    -f ${ExpressMongoDBPath}docker-compose.yaml \
+    -f ${ExpressMongoDBPath}docker-compose.prod.yaml \
     up -d \
     --build \
     # runs only one container
     # --no-deps expressdocker \
 
+
 if [[ $CLEAR = "prune" ]];
     then 
         docker image prune
         docker volume prune
+        docker network prune
 fi
-
 
