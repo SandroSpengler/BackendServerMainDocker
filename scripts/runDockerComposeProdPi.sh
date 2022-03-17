d3ab2998f453ec5c43c4eddcb0a9f95988466c2b
sandro spengler <sandro.spengler@web.de>
2022-03-17 16:58:24 +0100

    Changed the way docker-compose rebuilds

--- scripts/runDockerComposeProdPi.sh
+++ scripts/runDockerComposeProdPi.sh
@@ -1,30 +1,55 @@
 #1. Build and Runs the Docker-Compose Envoirment
 #2. Clears unnecessary images
-CLEAR=$1
+BUILD=$1
 
-docker-compose \
-    -f ${MainDockerPath}docker-compose.yaml \
-    -f ${MainDockerPath}docker-compose.prod.yaml \
-    -f ${ExpressMongoDBPath}docker-compose.yaml \
-    -f ${ExpressMongoDBPath}docker-compose.pi.yaml \
-    -f ${LeagueAbuserBackend}docker-compose.yaml \
-    -f ${LeagueAbuserBackend}docker-compose.pi.yaml \
-    down --remove-orphans -v
+# if [[ $BUILD = "" ]];
+#     then 
+#     echo "123"
+# fi
 
-docker-compose \
-    -f ${MainDockerPath}docker-compose.yaml \
-    -f ${MainDockerPath}docker-compose.prod.yaml \
-    -f ${ExpressMongoDBPath}docker-compose.yaml \
-    -f ${ExpressMongoDBPath}docker-compose.pi.yaml \
-    -f ${LeagueAbuserBackend}docker-compose.yaml \
-    -f ${LeagueAbuserBackend}docker-compose.pi.yaml \
-    up -d \
-    --build \
-    # runs only one container
-    # --no-deps expressdocker \
+WebsiteMainDocker="/c/Users/Sandro/Dropbox/0_Webdevelopment/Docker/Live/BackendServerMainDocker/"
+WebsiteExpressMongo="/c/Users/Sandro/Dropbox/0_Webdevelopment/Projects/NodeJs/Live/PersonalWebsiteExpressMongoDB/"
+LeagueAbuserBackend="/c/Users/Sandro/Dropbox/0_Webdevelopment/Projects/NodeJs/Live/LeagueAbuserBackend/"
+
+if [[ $BUILD = "full" ]];
+    then 
+        docker-compose \
+            -f ${WebsiteMainDocker}docker-compose.yaml \
+            -f ${WebsiteMainDocker}docker-compose.dev.yaml \
+            -f ${WebsiteExpressMongo}docker-compose.yaml \
+            -f ${WebsiteExpressMongo}docker-compose.dev.yaml \
+            -f ${LeagueAbuserBackend}docker-compose.yaml \
+            -f ${LeagueAbuserBackend}docker-compose.dev.yaml \
+            down --remove-orphans -v
 
 
-if [[ $CLEAR = "prune" ]];
+        docker-compose \
+            -f ${WebsiteMainDocker}docker-compose.yaml \
+            -f ${WebsiteMainDocker}docker-compose.dev.yaml \
+            -f ${WebsiteExpressMongo}docker-compose.yaml \
+            -f ${WebsiteExpressMongo}docker-compose.dev.yaml \
+            -f ${LeagueAbuserBackend}docker-compose.yaml \
+            -f ${LeagueAbuserBackend}docker-compose.dev.yaml \
+            up -d \
+            --build \
+            # runs only one container
+            # --no-deps expressdocker \
+    else
+        docker-compose \
+            -f ${WebsiteMainDocker}docker-compose.yaml \
+            -f ${WebsiteMainDocker}docker-compose.dev.yaml \
+            -f ${WebsiteExpressMongo}docker-compose.yaml \
+            -f ${WebsiteExpressMongo}docker-compose.dev.yaml \
+            -f ${LeagueAbuserBackend}docker-compose.yaml \
+            -f ${LeagueAbuserBackend}docker-compose.dev.yaml \
+            up -d \
+            --build $BUILD \
+            # runs only one container
+            # --no-deps expressdocker \
+fi
+
+
+if [[ $BUILD = "prune" ]];
     then 
         docker image prune
         docker volume prune
