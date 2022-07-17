31ca0534913d4a232e9ec73b04d2c44df17f763e
sandro spengler <sandro.spengler@web.de>
2022-07-17 10:02:44 +0200

    Improved Production script

--- scripts/runDockerComposeProd.sh
+++ scripts/runDockerComposeProd.sh
@@ -1,53 +1,61 @@
+#!/bin/bash
 #1. Build and Runs the Docker-Compose Envoirment
 #2. Clears unnecessary images
 BUILD=$1
 
-if [[ $BUILD = "full" ]];
-    then
-        docker-compose \
-            -f ${MainDockerPath}docker-compose.yaml \
-            -f ${MainDockerPath}docker-compose.prod.yaml \
-            -f ${LeagueAbuserBackend}docker-compose.yaml \
-            -f ${LeagueAbuserBackend}docker-compose.pi.yaml \
-            down --remove-orphans -v
-
-
-        docker-compose \
-            -f ${MainDockerPath}docker-compose.yaml \
-            -f ${MainDockerPath}docker-compose.prod.yaml \
-            -f ${LeagueAbuserBackend}docker-compose.yaml \
-            -f ${LeagueAbuserBackend}docker-compose.pi.yaml \
-            up -d \
-            --build \
-            # runs only one container
-            # --no-deps expressdocker \
-    else
-        docker-compose \
-            -f ${MainDockerPath}docker-compose.yaml \
-            -f ${MainDockerPath}docker-compose.prod.yaml \
-            -f ${LeagueAbuserBackend}docker-compose.yaml \
-            -f ${LeagueAbuserBackend}docker-compose.pi.yaml \
-            up -d \
-            --build $BUILD \
-            # runs only one container
-            # --no-deps expressdocker \
+if [ -z "$BUILD" ];
+then
+    echo "No Build Service provided - Building all services"
+    BUILD="full"
+else
+    echo "Build is: $BUILD"
 fi
 
+if [[ $BUILD = "full" ]];
+then
+    docker-compose \
+    -f ${MainDockerPath}docker-compose.yaml \
+    -f ${MainDockerPath}docker-compose.prod.yaml \
+    -f ${LeagueAbuserBackend}docker-compose.yaml \
+    -f ${LeagueAbuserBackend}docker-compose.pi.yaml \
+    down --remove-orphans -v
+    
+    
+    docker-compose \
+    -f ${MainDockerPath}docker-compose.yaml \
+    -f ${MainDockerPath}docker-compose.prod.yaml \
+    -f ${LeagueAbuserBackend}docker-compose.yaml \
+    -f ${LeagueAbuserBackend}docker-compose.pi.yaml \
+    up -d \
+    --build \
+    # runs only one container
+    # --no-deps expressdocker \
+else
+    docker-compose \
+    -f ${MainDockerPath}docker-compose.yaml \
+    -f ${MainDockerPath}docker-compose.prod.yaml \
+    -f ${LeagueAbuserBackend}docker-compose.yaml \
+    -f ${LeagueAbuserBackend}docker-compose.pi.yaml \
+    up -d \
+    --build $BUILD \
+    # runs only one container
+    # --no-deps expressdocker \
+fi
 
 if [[ $BUILD = "down" ]];
-    then
-        docker-compose \
-            -f ${MainDockerPath}docker-compose.yaml \
-            -f ${MainDockerPath}docker-compose.prod.yaml \
-            -f ${LeagueAbuserBackend}docker-compose.yaml \
-            -f ${LeagueAbuserBackend}docker-compose.pi.yaml \
-            down --remove-orphans -v
+then
+    docker-compose \
+    -f ${MainDockerPath}docker-compose.yaml \
+    -f ${MainDockerPath}docker-compose.prod.yaml \
+    -f ${LeagueAbuserBackend}docker-compose.yaml \
+    -f ${LeagueAbuserBackend}docker-compose.pi.yaml \
+    down --remove-orphans -v
 fi
 
 if [[ $BUILD = "prune" ]];
-    then
-        docker image prune
-        docker volume prune
-        docker network prune
+then
+    docker image prune
+    docker volume prune
+    docker network prune
 fi
 
